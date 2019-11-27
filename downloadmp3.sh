4f7f483d1be4ed66120ce8f606e1578f1de45521
raf <rafutek@protonmail.com>
2019-11-26 20:29:22 -0500

    remove expressions from song title

--- downloadmp3.sh
+++ downloadmp3.sh
@@ -32,14 +32,20 @@ Options:
 	-i /absolute/path/to/image
 			Set the cover image
 	-d /absolute/path/to/destination/directory/
-			Set the destination directory to download the mp3 file(s)	
-	-r "exp1/exp2/[...]/expn"
-			Indicates the expressions to remove in the song name
+			Set the destination directory to create the arborescence and download the mp3 file(s)	
+	-r \"exp1/exp2/[...]/expn\"
+			Indicate the expressions to remove in the video title to create the song name
+			(remove ' - ' by default)
+
+Warnings:
+	> This script must be executed in a bash shell
+	> Image and url are required
+	> Paths must not contain spaces
 	"
 }
 
 usage() {
-	echo "Usage: ${0} [OPTIONS] -u URL"
+	echo "Usage: ${0} [OPTIONS] -i /absolute/path/image -u URL"
 }
 
 exit_abnormal(){
@@ -63,58 +69,41 @@ while getopts ":ha:A:g:y:i:d:u:r:" options; do
 		h)
 			help
 			exit 0
-		;;
+			;;
 		a)
 			ARTIST=${OPTARG}
-		;;
+			;;
 		A)
 			ALBUM=${OPTARG}
-		;;
+			;;
 		g)
 			GENRE=${OPTARG}
-		;;
+			;;
 		y)
 			YEAR=${OPTARG}
-		;;
+			;;
 		i)
 			IMG=${OPTARG}
-		;;
+			;;
 		d)
 			DEST=${OPTARG}
-		;;	
+			;;	
 		u)
 			URL=${OPTARG}
-		;;
+			;;
 		r)
 			ALL_EXPR=${OPTARG}
-		;;
+			;;
 		*)
 			exit_abnormal
-		;;
+			;;
 	esac
 done
 
-iterate=true
-while [ ${iterate} = true ]
-do
-	case ${ALL_EXPR} in
-		*"/"*)
-			;;
-		*)
-			iterate=false
-			;;
-	esac
-
-	exp=${ALL_EXPR##*/}
-	ALL_EXPR=${ALL_EXPR%/*}
-	echo "${exp}"
-done
-
-exit 0
 
 #-----------------------------------Make some verifications and modifications if needed
 
-if [ "${URL}" = "" ]; then
+if [ "${URL}" = "" ] || [ "${IMG}" = "" ]; then
 	exit_abnormal
 fi
 
@@ -147,17 +136,37 @@ if [ "${YEAR}" = "" ] ; then
 	YEAR="0000"
 fi
 
+#----------------------------------Create array of expressions to remove from files
+if [ "${ALL_EXP}" = "" ] ; then
+	exparray=()
+	iterate=true
+	while [ ${iterate} = true ]
+	do
+		case ${ALL_EXPR} in
+			*"/"*)
+				;;
+			*)
+				iterate=false
+				;;
+		esac
 
-#--------------------Create folder arborescence
+		exp=${ALL_EXPR##*/}
+		ALL_EXPR=${ALL_EXPR%/*}
+		exparray+=("${exp}")
+	done
+fi
+
+
+#----------------------------Create folder arborescence
 cd ${DEST}
 tree="${ARTIST}/${ALBUM}"
 mkdir -p "${tree}"
 cd "${tree}"
 
-###--------------------------------Delete mp3
+###-------------------------------------------Delete mp3
 if [ $(ls -1 *.mp3 | wc -l) -ne 0 ]; then
 	ls -lh 
-	read -p "The program is going to remove all mp3 files in \"${DEST}/${tree}\" before downloading, are you sure ? (y/n) : " input 
+	read -n1 -p "The program is going to remove all mp3 files in \"${DEST}/${tree}\" before downloading, are you sure ? (y/n) : " input 
 	case ${input} in
 		[yY])
 	;;
@@ -166,12 +175,12 @@ if [ $(ls -1 *.mp3 | wc -l) -ne 0 ]; then
 	;;
 	esac
 
-	echo "Delete all mp3 files in folder..."
+	echo -e "\n\rDelete all mp3 files in folder..."
 	rm -f *.mp3
 fi
 
 ###--------------------------------------------------Download mp3 file(s)
-echo "\nStart downloading music from url..."
+echo -e "\nStart downloading music from url..."
 youtube-dl -x --audio-format mp3 ${URL} -o '%(title)s.mp3'
 
 
@@ -179,14 +188,22 @@ youtube-dl -x --audio-format mp3 ${URL} -o '%(title)s.mp3'
 i=0
 for entry in *".mp3"
 do
-	dest=$(echo ${entry} | tr ' ' '_')
+
+	newname="${entry}"
+	for exp in "${exparray[@]}"
+	do
+		if [ ${#exp} -gt 0 ]; then
+			newname=$( echo "${newname}" | sed "s/${exp}//gI" )
+		fi
+	done
+	
+	newname=$( echo "${newname}" | sed "s/ - //g" )
+	dest=$( echo "${newname}" | sed "s/ /_/g" )
+	echo "new name: ${newname}, dest name: ${dest}"
+
 	src="_${dest}"
-	mv "${filename}" "${src}"
-	if [ "${IMG}" != "" ]; then
-		ffmpeg -i ${src} -i ${IMG} -map_metadata 0 -map 0 -map 1 ${dest}
-	else
-		mv "${src}" "${dest}"
-	fi
+	mv "${entry}" "${src}"
+	ffmpeg -i ${src} -i "${IMG}" -map_metadata 0 -map 0 -map 1 ${dest}
 	rm -f ${src}
 	i=$((i+1))
 	mid3v2 -a "${ARTIST}" -A "${ALBUM}" -g "${GENRE}" -y ${YEAR} -T "${i}" ${dest}
