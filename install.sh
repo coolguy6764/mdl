2818b2ebce0977d957e14d425a9dc91532ddaf58
raf <rafutek@protonmail.com>
2019-11-27 23:53:05 -0500

    install dependent packages

--- install.sh
+++ install.sh
@@ -2,9 +2,46 @@
 
 #!/bin/bash
 
+
+trap "error 'installation stopped'" 2
+
+error() {
+	echo "${1}" >&2
+	exit 1
+}
+
+
+missing_do_() {
+	hash ${1}
+	if [ ${?} -ne 0 ];then
+		shift
+		for cmd in "${@}"
+		do
+			eval ${cmd}
+		done
+	fi
+}
+
+install_curl="apt-get install curl"
+install_ydl="missing_do_ curl '${install_curl}'; 
+		curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl; 
+		chmod a+rx /usr/local/bin/youtube-dl"
+install_ffmpeg="apt-get install ffmpeg"
+install_mid3v2="apt-get install python-mutagen"
+
+echo -e "\nCheck for youtube-dl..."
+missing_do_ youtube-dl "${install_ydl}"
+
+echo -e "\nCheck for ffmpeg..."
+missing_do_ ffmpeg "${install_ffmpeg}"
+
+echo -e "\nCheck for mid3v2..."
+missing_do_ mid3v2 "${install_mid3v2}"
+
+
 script="downloadmp3"
 chmod +x "${script}.sh"
 cp "${script}.sh" "/usr/local/bin/${script}"
 if [ ${?} -eq 0 ];then
-	echo "${script} installé"
+	echo -e "\n${script} installé"
 fi
