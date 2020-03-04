8932588f50d23e48a2b27566bf64e8811a753ff3
linur <raphaelroy@tutanota.com>
2020-03-04 02:07:06 -0500

    add utils

---
+++ utils.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+BIN_PATH="/usr/local/bin/"
+ARCH="Arch"
+DEBIAN="Debian"
+
+be_root() {
+	[ "$(id -u)" -ne 0 ] && 
+		echo "Please run as root"  &&
+		exit 1
+}
+
+trap_signals() {
+	trap "error 'Installation stopped'" 2
+}
+error() {
+	echo "${1}" >&2
+	exit 1
+}
+
+get_OS() {
+	OS=""
+	[ "${OS}" = "" ] && [ "$(is_present "pacman")" -eq 0 ] && 
+		OS="${ARCH}"
+	[ "${OS}" = "" ] && [ "$(is_present "apt")" -eq 0 ] && 
+		OS="${DEBIAN}"
+	[ "${OS}" = "" ] && error "Sorry, this script works only for Arch and Debian based systems... "
+	echo "${OS}"
+}
+
+missing_do_() {
+	echo "Checking for ${1}..."
+	if [ "$(is_present "${1}")" -eq 1 ] ; then
+		shift
+		for cmd in "${@}"
+		do
+			eval "${cmd}"
+		done
+	fi
+}
+
+is_present() {
+	if command -v "${1}" >"/dev/null"; then
+		echo 0
+	else
+		echo 1
+	fi
+}
+
+
